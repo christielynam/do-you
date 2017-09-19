33ae3496dc446eda6f409d17a2ccb159a7c12c6b
Margo Schaedel <margoschaedel@gmail.com>
2017-09-19 10:47:03 -0600

    Update actions test boilerplate

---
+++ test/actions.test.js
@@ -0,0 +1,6 @@
+import * as action from '../actions';
+import mockPersonalityData from './mockPersonalityData';
+import fetchMock from 'fetch-mock';
+
+import publicKey from '../utils/keys';
+import secretKey from '../utils/keys';
