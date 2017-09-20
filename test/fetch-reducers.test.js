d9412c071ea636461ba5cf69a5ef00ab22691736
Margo Schaedel <margoschaedel@gmail.com>
2017-09-20 07:47:03 -0600

    Finish reducer testing

--- test/fetch-reducers.test.js
+++ test/fetch-reducers.test.js
@@ -1,67 +1,137 @@
-import * as reducer from '../lib/reducers/database-reducers';
-import mockTests from '../test-helpers/mockTestsData.js';
+import * as reducer from '../lib/reducers/fetch-reducers';
+import mockPersonalities from '../test-helpers/mockPersonalityData.js';
+import mockSlides from '../test-helpers/mockAssessmentSlides.js';
+import mockResults from '../test-helpers/mockAssessmentResults.js';
 
-describe('storing user', () => {
+
+describe('fetch has erred', () => {
   it('should have a default state', () => {
-    const defaultUser = null
-    expect(reducer.user(undefined, {})).toEqual(defaultUser)
+    const defaultState = false
+    expect(reducer.hasErred(undefined, {})).toEqual(defaultState)
   })
 
-  it('should remove a user', () => {
-    const removeUserAction = {
-      type: 'REMOVE_USER',
-      user: null
+  it('should set state to true if fetch erred', () => {
+    const hasErredAction = {
+      type: 'FETCH_HAS_ERRED',
+      hasErred: true
     }
-    const removedUser = null
-    expect(reducer.user(undefined, removeUserAction)).toEqual(removedUser)
+    expect(reducer.hasErred(undefined, hasErredAction)).toEqual(true)
+  })
+})
+
+describe('fetch is loading', () => {
+  it('should have a default state', () => {
+    const defaultState = false
+    expect(reducer.isLoading(undefined, {})).toEqual(defaultState)
   })
 
-  it('should store a user successfully', () => {
-    const userAction = {
-      type: 'STORE_USER',
-      user: {
-        id: 6,
-        name: 'bo',
-        email: 'bo@bo.com',
-        password: 'bo',
-        created_at: '2017-09-19T00:58:18.083Z',
-        updated_at: '2017-09-19T00:58:18.083Z'
+  it('should set state to true if fetch is loading', () => {
+    const isLoadingAction = {
+      type: 'FETCH_IS_LOADING',
+      isLoading: true
+    }
+    expect(reducer.isLoading(undefined, isLoadingAction)).toEqual(true)
+  })
+})
+
+describe('fetching an assessment', () => {
+  it('should have a default state', () => {
+    const defaultState = {}
+    expect(reducer.fetchedAssessment(undefined, {})).toEqual(defaultState)
+  })
+
+  it('should remove a fetched assessment', () => {
+    const removeAction = {
+      type: 'REMOVE_FETCHED_ASSESSMENT',
+      fetchedAssessment: {}
+    }
+    expect(reducer.fetchedAssessment(undefined, removeAction)).toEqual({})
+  })
+
+  it('should fetch an assessment successfully', () => {
+    const fetchedAssessmentAction = {
+      type: 'FETCH_ASSESSMENT_SUCCESS',
+      fetchedAssessment: {
+        id: "e34c6cbb-9f6d-4008-8e9f-cdbce5ffcb80",
+        deck_id: "introvert-extrovert",
+        completed_at: null,
+        created_at: 1505913897188,
+        locale_key: "en-US",
+        profile_id: "b93213c3-7be3-4015-807a-a67f1d68a8b1",
+        recommendation_id: null
       }
     }
 
     const expectedResponse = {
-        id: 6,
-        name: 'bo',
-        email: 'bo@bo.com',
-        password: 'bo',
-        created_at: '2017-09-19T00:58:18.083Z',
-        updated_at: '2017-09-19T00:58:18.083Z'
+      id: "e34c6cbb-9f6d-4008-8e9f-cdbce5ffcb80",
+      deck_id: "introvert-extrovert",
+      completed_at: null,
+      created_at: 1505913897188,
+      locale_key: "en-US",
+      profile_id: "b93213c3-7be3-4015-807a-a67f1d68a8b1",
+      recommendation_id: null
     }
 
-    expect(reducer.user(undefined, userAction)).toEqual(expectedResponse)
+    expect(reducer.fetchedAssessment(undefined, fetchedAssessmentAction)).toEqual(expectedResponse)
   })
 })
 
-describe('storing tests', () => {
+describe('fetch personalities', () => {
   it('should have a default state', () => {
-    const defaultTests = null
-    expect(reducer.tests(undefined, {})).toEqual(defaultTests)
+    const defaultState = []
+    expect(reducer.fetchedPersonalities(undefined, {})).toEqual(defaultState)
   })
 
-  it('should remove tests', () => {
-    const removeTestsAction = {
-      type: 'REMOVE_TESTS',
-      tests: null
+  it('should fetch personalities successfully', () => {
+    const fetchPersonalitiesAction = {
+      type: 'FETCH_PERSONALITIES_SUCCESS',
+      fetchedPersonalities: mockPersonalities
     }
-    const removedTests = null
-    expect(reducer.tests(undefined, removeTestsAction)).toEqual(removedTests)
+    expect(reducer.fetchedPersonalities(undefined, fetchPersonalitiesAction)).toEqual(mockPersonalities)
+  })
+})
+
+describe('fetch assessment slides', () => {
+  it('should have a default state', () => {
+    const defaultState = []
+    expect(reducer.assessmentSlides(undefined, {})).toEqual(defaultState)
   })
 
-  it('should store tests successfully', () => {
-    const testAction = {
-      type: 'STORE_TESTS',
-      tests: mockTests
+  it('should remove slides successfully', () => {
+    const removeSlides = {
+      type: 'REMOVE_SLIDES',
+      assessmentSlides: []
     }
-    expect(reducer.tests(undefined, testAction)).toEqual(mockTests)
+    expect(reducer.assessmentSlides(undefined, removeSlides)).toEqual([])
+  })
+
+  it('should fetch slides successfully', () => {
+    const slidesAction = {
+      type: 'DISPLAY_ASSESSMENT_SUCCESS',
+      assessmentSlides: mockSlides
+    }
+    expect(reducer.assessmentSlides(undefined, slidesAction)).toEqual(mockSlides)
+  })
+})
+
+describe('fetch assessment results', () => {
+  it('should have a default state', () => {
+    expect(reducer.assessmentResults(undefined, {})).toEqual(null)
+  })
+
+  it('should remove results successfully', () => {
+    const removeResults = {
+      type: 'REMOVE_TEST_RESULTS',
+      assessmentResults: null
+    }
+    expect(reducer.assessmentResults(undefined, removeResults)).toEqual(null)
+  })
+
+  it('should fetch results successfully', () => {
+    const resultsAction = {
+      type: 'DISPLAY_RESULTS_SUCCESS',
+      assessmentResults: mockResults
+    }
+    expect(reducer.assessmentResults(undefined, resultsAction)).toEqual(mockResults)
   })
 })
